3230d363c56f226366e5b9c048591a74d4417862
Jean-Luc Deladrière <deladriere@gmail.com>
2019-06-05 11:54:21 +0200

    adding LTC6903 clock for midi

--- Robo_vox/Software/Robovox_arduino_midi/src/main.cpp
+++ Robo_vox/Software/Robovox_arduino_midi/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "MCP23008.h" //from http://gtbtech.com/?p=875
 #include "Wire.h"
-#include "DS1077.h"
+
 #include <MIDIUSB.h>
 
 #define MCP23008_ADDR 0x40 // TODO: corriger ? en fait c'est 20
@@ -22,7 +22,12 @@
 
 #define AR 5
 #define RW 6
-#define RST 7
+
+// LTC6903
+const int SEN = 4;            //Serial Enable for LTC6903
+const int SCLK = PIN_SPI_SCK; //Serial Clock for clocking in data
+const int SDI = PIN_SPI_MOSI; //Serial Data Input, D15 first
+int Word = 0;                 //shifting word sent to ltc6903
 
 /*
    ██████  ██████       ██ ███████  ██████ ████████ ███████
@@ -37,15 +42,38 @@ MCP23008 SC02(MCP23008_ADDR);
 int i;
 byte hello[] = {44, 10, 32, 17, 35, 0, 0x23, 0x1C, 0x20, 0x25};
 
-
-
-void Reset()
+void Transfer(word sdi)
 {
-  digitalWrite(RST, LOW);
-  delayMicroseconds(1);
-  digitalWrite(RST, HIGH);
+  int value = 0; //set D15 test value to zero;
+
+  digitalWrite(SEN, LOW);       //set Serial Enable True at ltc6903
+  for (int i = 0; i != 16; i++) //Got 16 bits to clock in
+  {
+    value = sdi & 32768; //Strip for testing if D15
+    if (value != 0)      // 1?
+    {
+      digitalWrite(SDI, HIGH); //Set SDI to TRUE
+    }
+    else
+    {
+      digitalWrite(SDI, LOW); //Else set to LOW
+    }
+    digitalWrite(SCLK, LOW);  //Drop the clock line
+    digitalWrite(SCLK, HIGH); //Trigger in the SDI low
+    sdi <<= 1;                //Shift next bit into D15 position
+  }
+  digitalWrite(SEN, HIGH); //deselect serial transfer
+  digitalWrite(SDI, HIGH); //put data high as initial state
 }
 
+void ltc6903(int oct, unsigned int dac)
+{
+  Word = oct;     // do OCT first
+  Word <<= 12;    // move it to D15
+  dac <<= 2;      //align the DAC value
+  Word |= dac;    //OR it in, leave CN1 and CN0 at zero
+  Transfer(Word); //Send word as sdi to ltc6903
+}
 void Strobe()
 {
   digitalWrite(RW, LOW);
@@ -71,19 +99,18 @@ void Command(byte registre, byte value)
   digitalWrite(RS0, registre & B00000001);
   digitalWrite(RS1, registre & B00000010);
   digitalWrite(RS2, registre & B00000100);
- 
+
   SC02.writeGPIO(value);
   delayMicroseconds(1);
-  
+
   Strobe();
 }
 
 void noteOn(byte channel, byte pitch, byte velocity)
 {
   digitalWrite(LED_BUILTIN, HIGH);
-  Command(1, map(analogRead(A1), 0, 255, 0, 255));
+  //  Command(1, map(analogRead(A1), 0, 255, 0, 255));
   Phoneme(pitch);
-  
 }
 
 void noteOff(byte channel, byte pitch, byte velocity)
@@ -101,8 +128,7 @@ void controlChange(byte channel, byte control, byte value)
 void setup()
 {
   Wire.begin();
-  analogReadResolution(8);
-  DS1077_init(54); // 56 = 1.8 MHz
+  ltc6903(10, 11); //Set pitch to default value
 
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -112,29 +138,35 @@ void setup()
 
   pinMode(RW, OUTPUT);
   pinMode(AR, INPUT);
-  pinMode(RST, OUTPUT);
+  //pinMode(RST, OUTPUT);
 
-  digitalWrite(RST, HIGH);
+  //digitalWrite(RST, HIGH);
   digitalWrite(RW, HIGH);
 
-  delay(100);
+  delay(1000);
   //Reset();
   SC02.writeIODIR(0x0);
 
-  Command(0, 192); // load DR1 DR2 bit to 1 (to activate A/R request mode) (192)
   Command(3, 128); //Control bit to 1 (128)
-  //Command(3, 0);   // Control bit to 0 (to activate A/R request mode by going to 0) (0)
-  Command(3, 110); // Set articulation to normal and amplitude to maximum (116)
-  Command(4, 240); // Set Filter frequency to normal (231)
-  Command(2, 200); // Set Speech rate to normal (168)
-  Command(1, 127);
+  Command(0, 192); // load DR1 DR2 bit to 1 (to activate A/R request mode) (192)
+  Command(3, 0);   // //Control bit to 0
+
+  Command(3, B01111111); // Set articulation to normal and amplitude to maximum  & CTL to 0
+  Command(4, 240);       // Set Filter frequency to normal (231)
+  Command(2, 200);       // Set Speech rate to normal (168)
+  Command(1, 127);       // inflection
+
+  for (int y = 0; y < 11; y++)
+  {
+    Phoneme(hello[y]);
+  }
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
   midiEventPacket_t rx = MidiUSB.read();
-  
+
   switch (rx.header)
   {
   case 0:
@@ -165,6 +197,6 @@ void loop()
     break;
 
   default:
-  break;
+    break;
   }
 }

16eb83fa3fd905640ce82519d88cd466ec2315df
Jean-Luc Deladrière <deladriere@gmail.com>
2021-03-27 11:24:53 +0100

    Sino : rename custom_hwids.py

---
+++ Sino_vox/Software/Sinovox/custom_hwids.py
@@ -0,0 +1,22 @@
+Import("env")
+
+board_config = env.BoardConfig()
+print(env.BoardConfig().get("build.hwids")[0][0])
+
+board_config.update("vendor", "Polaxis")
+print(env.BoardConfig().get("vendor"))
+board_config.update("build.usb_product", "Sino")
+print(env.BoardConfig().get("build.usb_product"))
+
+# should be array of VID:PID pairs
+board_config.update(
+    "build.hwids", [["0x239A", "0x0105"]]
+)  # VID PID 2341 arduino 0243 sinovox 239A adafruit
+print(env.BoardConfig().get("build.hwids")[0][0])
+
+# emy 0100
+# Kaiwa 0101
+# Mea 0102
+# Vax 0103
+# Robovox 0104
+# Sino 0105

0ae616f48ca129d7c3186be52d85c0b01e30468f
Jean-Luc Deladrière <deladriere@gmail.com>
2021-08-11 08:50:15 +0200

    Adding Speakjet

---
+++ SpeakJet_vox/Software/SpeakJet/custom_hwids.py
@@ -0,0 +1,23 @@
+Import("env")
+
+board_config = env.BoardConfig()
+print(env.BoardConfig().get("build.hwids")[0][0])
+
+board_config.update("vendor", "Polaxis")
+print(env.BoardConfig().get("vendor"))
+board_config.update("build.usb_product", "Speakjet")
+print(env.BoardConfig().get("build.usb_product"))
+
+# should be array of VID:PID pairs
+board_config.update(
+    "build.hwids", [["0x239A", "0x0106"]]
+)  # VID PID 2341 arduino 0243 sinovox 239A adafruit
+print(env.BoardConfig().get("build.hwids")[0][0])
+
+# Emy 0100
+# Kaiwa 0101
+# Mea 0102
+# Vax 0103
+# Robovox 0104
+# Sino 0105
+# Speakjet 0106
---
+++ SpeakJet_vox/Software/SpeakJet/src/main.ino
@@ -0,0 +1,373 @@
+#include <Arduino.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
+#include "SDU.h"
+#include <SD.h> // 868
+#include "OneButton.h"
+// voir first test arduino for better sound
+
+/*
+electronic font
+ ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄   ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ 
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌▐░▌   ▄   ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀█░█▀▀ ▐░▌       ▐░▌▐░▌ ▐░▌░▌ ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀▀▀ 
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌     ▐░▌  ▐░▌       ▐░▌▐░▌▐░▌ ▐░▌▐░▌▐░▌       ▐░▌▐░▌     ▐░▌  ▐░▌          
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄█░▌▐░▌░▌   ▐░▐░▌▐░▌       ▐░▌▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄ 
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░▌ ▐░░▌     ▐░░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌
+ ▀         ▀  ▀         ▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀       ▀▀  ▀         ▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀ 
+                                                                                                        
+*/
+
+#define BUSY 38
+#define SW0 0
+#define SW1 1
+
+#define GREEN_LED 12
+#define RED_LED 13
+
+#define PIN_SPEAKING 11
+#define PIN_BHF 9
+
+#define IS_SPEAKING digitalRead(PIN_SPEAKING)
+
+#define ON 0
+#define OFF 1
+
+#define GATE 2
+#define PUSH 3
+
+#define ROTA 6
+#define ROTB 7
+
+#define MODE_AUTO 1
+#define MODE_TRIG 2
+#define MODE_GATE 3
+
+/*
+ ▄               ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄   ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░▌             ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+ ▐░▌           ▐░▌ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ 
+  ▐░▌         ▐░▌  ▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌          
+   ▐░▌       ▐░▌   ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ 
+    ▐░▌     ▐░▌    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+     ▐░▌   ▐░▌     ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀█░█▀▀      ▐░▌     ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀█░▌
+      ▐░▌ ▐░▌      ▐░▌       ▐░▌▐░▌     ▐░▌       ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌                    ▐░▌
+       ▐░▐░▌       ▐░▌       ▐░▌▐░▌      ▐░▌  ▄▄▄▄█░█▄▄▄▄ ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄█░▌
+        ▐░▌        ▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+         ▀          ▀         ▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ 
+                                                                                                                                                                                                      
+*/
+
+#define VERSION 0.1
+
+int mainState;
+int voiceState;
+
+byte null = 0;
+byte mode;
+bool triggered;
+bool pressed;
+bool longPressed;
+
+volatile bool rotF;              // because use in rot
+volatile int interruptCount = 0; // The rotary counter
+
+unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+unsigned long debounceDelay = 300;
+
+char SayIt[] = {20, 96, 21, 114, 22, 88, 23, 5, 145, 131, 193, 6, 140, 154, 196, 6, 148, 7, 137, 7, 164, 18, 171, 136, 193};
+
+char *mainFunctions[] = {
+    "Voice",
+    "Synth",
+    "Code",
+    "MIDI",
+    "",
+    "",
+    ""};
+
+int numFunctions = (sizeof(mainFunctions) / sizeof(mainFunctions[0]));
+int function;
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▀▀▀▀▀█░█▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ 
+▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌    ▐░▌          ▐░▌               ▐░▌     ▐░▌          
+▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌     ▐░▌    ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌               ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ 
+▐░▌       ▐░▌▐░░░░░░░░░░▌      ▐░▌    ▐░░░░░░░░░░░▌▐░▌               ▐░▌     ▐░░░░░░░░░░░▌
+▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌     ▐░▌    ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌               ▐░▌      ▀▀▀▀▀▀▀▀▀█░▌
+▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌    ▐░▌          ▐░▌               ▐░▌               ▐░▌
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▄▄▄▄▄█░▌    ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌      ▄▄▄▄▄▄▄▄▄█░▌
+▐░░░░░░░░░░░▌▐░░░░░░░░░░▌▐░░░░░░░▌    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌
+ ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀      ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀ 
+*/
+
+auto &SpeakJet = Serial5;
+SSD1306AsciiWire display;
+OneButton button(PUSH, true);
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ 
+▐░▌          ▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌               ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌          
+▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌▐░▌ ▐░▌   ▐░▌▐░▌               ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌▐░▌               ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░▌   ▐░▌ ▐░▌▐░▌               ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░▌   ▐░▌ ▐░▌ ▀▀▀▀▀▀▀▀▀█░▌
+▐░▌          ▐░▌       ▐░▌▐░▌    ▐░▌▐░▌▐░▌               ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░▌    ▐░▌▐░▌          ▐░▌
+▐░▌          ▐░█▄▄▄▄▄▄▄█░▌▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌      ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░▌     ▐░▐░▌ ▄▄▄▄▄▄▄▄▄█░▌
+▐░▌          ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌
+ ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀ 
+                                                                                                                                                                                                                        
+*/
+
+void longPress()
+{
+
+        Serial.println("longPress...");
+        longPressed = true;
+        display.clear();
+        delay(1000);
+        mainState = 0;
+        rotF = 1;
+} // longPress
+
+void displayFunctionList(int p)
+{
+
+        for (int i = 0; i < 4; i++)
+        { // nbr of lines to display
+
+                display.setCursor(13, i * 8); //
+                String str2 = mainFunctions[i + p];
+                display.clearToEOL();
+                display.println(str2);
+        }
+}
+void initDisplay()
+{
+        Wire.begin();
+        Wire.setClock(1500000L);              // speed the display to the max
+        display.begin(&Adafruit128x64, 0x3C); // initialize with the I2C addr 0x3D (for the 128x64)
+        display.clear();
+        display.set1X();
+        display.setFont(fixed_bold10x15);
+        display.setRow(0);
+        Wire.setClock(1500000L);
+}
+void Rotary()
+{
+        if (digitalRead(ROTA))
+        {
+                if (digitalRead(ROTB))
+                {
+                        interruptCount--;
+                }
+                else
+                {
+                        interruptCount++;
+                }
+                rotF = 1;
+        }
+}
+// MIDI
+void voiceSynth()
+{
+        //int delayLength = 100; //This delay seems to give a constant stream without overloading the buffer too fast. Tweak to suit.
+        int delayLength = map(analogRead(4), 0, 1023, 200, 10);
+        int phoneme = random(128, 180); //This range seemed to sound best but feel free to tweak.
+        phoneme = map(analogRead(6), 0, 1023, 128, 180);
+        //Serial.println(phoneme);
+        int pitch = map(analogRead(1), 0, 1023, 0, 255);       //Mapped to range of available pitch values.
+        int bend = map(analogRead(2), 0, 1023, 15, 0);         //Mapped to range of available bend values.
+        int speechSpeed = map(analogRead(3), 0, 1023, 0, 127); //Mapped to range of available speedvalues.
+        int volume = map(analogRead(5), 0, 1023, 127, 0);
+
+        //Serial5.write(null);
+        Serial5.write(0x15);                                //Send speed command
+        Serial5.write(map(speechSpeed, 0, 127, 0x7F, 0x0)); //Send speed value mapped to hex
+        Serial5.write(0x16);                                //Send pitch command
+        Serial5.write(map(pitch, 0, 255, 0xFF, 0x0));       //Send pitch value mapped to hex
+        Serial5.write(0x17);                                //Send bend command
+        Serial5.write(map(bend, 0, 15, 0x0, 0xF));          //Send bend value mapped to hex
+        Serial5.write(map(phoneme, 0, 255, 0x0, 0xFF));     //Send phoneme value mapped to hex
+        Serial5.write(20);                                  // volume
+        Serial5.write(volume);
+
+        delay(delayLength); //Delay for however long the delay length is
+        while (digitalRead(2))
+                ;
+        while (digitalRead(PIN_SPEAKING))
+                ;
+        switch (voiceState)
+        {
+        case 0:
+
+                break;
+
+        default:
+                break;
+        }
+}
+void getUser() // voir sinovox
+{
+        mode = digitalRead(SW0) + digitalRead(SW1) * 2;
+}
+
+void initPins()
+{
+        pinMode(ROTA, INPUT_PULLUP);
+        pinMode(ROTB, INPUT_PULLUP);
+
+        pinMode(SW0, INPUT_PULLUP);
+        pinMode(SW1, INPUT_PULLUP);
+        pinMode(PUSH, INPUT_PULLUP);
+        pinMode(GATE, INPUT_PULLUP);
+        pinMode(PIN_SPEAKING, INPUT_PULLUP);
+        pinMode(PIN_BHF, INPUT_PULLUP);
+        pinMode(RED_LED, OUTPUT);
+        pinMode(GREEN_LED, OUTPUT);
+
+        digitalWrite(12, HIGH);
+        digitalWrite(13, HIGH);
+}
+void Buffer()
+{
+        if (!digitalRead(PIN_BHF))
+        {
+                digitalWrite(GREEN_LED, OFF);
+        }
+        else
+        {
+                digitalWrite(GREEN_LED, ON);
+        }
+}
+
+void Speaking()
+{
+        if (!digitalRead(PIN_SPEAKING))
+        {
+
+                digitalWrite(BUSY, OFF);
+                digitalWrite(RED_LED, OFF);
+        }
+        else
+        {
+
+                digitalWrite(BUSY, ON);
+                digitalWrite(RED_LED, ON);
+        }
+}
+
+void justPressed()
+{
+        //voir gerty2 pour le debounce
+        pressed = true;
+}
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌
+▐░▌          ▐░▌               ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌
+▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░▌       ▐░▌▐░░░░░░░░░░░▌
+ ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀      ▐░▌     ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ 
+          ▐░▌▐░▌               ▐░▌     ▐░▌       ▐░▌▐░▌          
+ ▄▄▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌          
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌          
+ ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀                                                                           
+*/
+
+void setup()
+{
+        initPins();
+
+        analogReadResolution(10);
+
+        attachInterrupt(PIN_SPEAKING, Speaking, CHANGE);
+        attachInterrupt(PIN_BHF, Buffer, CHANGE);
+        attachInterrupt(PUSH, justPressed, FALLING);
+        attachInterrupt(ROTA, Rotary, CHANGE);
+
+        //  button.attachDuringLongPress(longPress);
+
+        SpeakJet.begin(9600); // set up SpeakJet library at 9600 bps
+
+        Serial.begin(115200);
+
+        SpeakJet.println(SayIt); // send it to the SpeakJet
+        initDisplay();
+        display.setCursor(0, 12);
+        display.println("SpeakJet");
+        delay(1000);
+        display.clear();
+        display.println(">");
+        pressed = false;
+        triggered = false;
+        mainState = 0;      // start with idle mode
+        rotF = 1;           // force first display
+        interruptCount = 0; // setup first display
+}
+/*
+ ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░▌          ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌
+▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌
+▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌
+▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌
+▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ 
+▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          
+▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌          
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌          
+ ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀           
+*/
+
+void loop()
+{
+        getUser();
+        button.tick();
+        if (button.getPressedTicks() > 500 && mainState)
+        {
+                display.clear();
+                delay(400);
+                interruptCount = 0;
+                mainState = 0;
+                rotF = 1;
+                pressed = false;
+        }
+        switch (mainState)
+        {
+        case 0: // idle
+                if (rotF)
+                {
+                        interruptCount = constrain(interruptCount, 0, numFunctions - 4);
+                        function = interruptCount;
+                        displayFunctionList(function);
+                        rotF = 0;
+                }
+                if (pressed)
+                {
+                        mainState = function + 1;
+                        display.clear();
+                        String str2 = mainFunctions[function];
+                        display.print(str2);
+                        pressed = false;
+                }
+                break;
+        case 1: // voice synth
+                //  voiceSynth();
+                break;
+        case 2: // synth
+                break;
+        case 3: // code
+                break;
+        case 4: // MIDI
+                break;
+        }
+}

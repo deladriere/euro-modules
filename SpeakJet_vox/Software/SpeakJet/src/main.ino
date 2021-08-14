852c6dd4c7e7e969b27872af3841dc18ebb3fa62
Jean-Luc Deladrière <deladriere@gmail.com>
2021-08-14 12:39:47 +0200

    making progress with Speakjet

--- SpeakJet_vox/Software/SpeakJet/src/main.ino
+++ SpeakJet_vox/Software/SpeakJet/src/main.ino
@@ -4,6 +4,7 @@
 #include "SDU.h"
 #include <SD.h> // 868
 #include "OneButton.h"
+#include "speakjet.h"
 // voir first test arduino for better sound
 
 /*
@@ -71,6 +72,7 @@ byte null = 0;
 byte mode;
 bool triggered;
 bool pressed;
+bool gated;
 bool longPressed;
 
 volatile bool rotF;              // because use in rot
@@ -82,7 +84,7 @@ unsigned long debounceDelay = 300;
 char SayIt[] = {20, 96, 21, 114, 22, 88, 23, 5, 145, 131, 193, 6, 140, 154, 196, 6, 148, 7, 137, 7, 164, 18, 171, 136, 193};
 
 char *mainFunctions[] = {
-    "Voice",
+    "Phoneme",
     "Synth",
     "Code",
     "MIDI",
@@ -92,6 +94,24 @@ char *mainFunctions[] = {
 
 int numFunctions = (sizeof(mainFunctions) / sizeof(mainFunctions[0]));
 int function;
+
+enum _EndLineCode
+{
+        endl
+};
+
+template <class T>
+inline Print &operator<<(Print &obj, T arg)
+{
+        obj.print(arg);
+        return obj;
+}
+
+inline Print &operator<<(Print &obj, _EndLineCode arg)
+{
+        obj.println();
+        return obj;
+}
 
 /*
  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
@@ -176,46 +196,79 @@ void Rotary()
         }
 }
 // MIDI
-void voiceSynth()
+void phonSynth()
 {
-        //int delayLength = 100; //This delay seems to give a constant stream without overloading the buffer too fast. Tweak to suit.
-        int delayLength = map(analogRead(4), 0, 1023, 200, 10);
-        int phoneme = random(128, 180); //This range seemed to sound best but feel free to tweak.
-        phoneme = map(analogRead(6), 0, 1023, 128, 180);
+
+        //     Serial << "voice " << voiceState << endl;
+
+        //int phoneme = random(128, 199); //This range seemed to sound best but feel free to tweak.
+        int phoneme = map(analogRead(6), 1023, 10, 128, 148);
+        phoneme = constrain(phoneme, 128, 199);
         //Serial.println(phoneme);
-        int pitch = map(analogRead(1), 0, 1023, 0, 255);       //Mapped to range of available pitch values.
-        int bend = map(analogRead(2), 0, 1023, 15, 0);         //Mapped to range of available bend values.
+        int pitch = map(analogRead(1), 1023, 10, 0, 255); //Mapped to range of available pitch values.
+        pitch = constrain(pitch, 0, 255);
+        int bend = map(analogRead(2), 0, 1023, 15, 0); //Mapped to range of available bend values.
+        bend = constrain(bend, 0, 15);
         int speechSpeed = map(analogRead(3), 0, 1023, 0, 127); //Mapped to range of available speedvalues.
+        speechSpeed = constrain(speechSpeed, 0, 127);
+        int repeat = map(analogRead(4), 1003, 0, 0, 10);
+        repeat = constrain(repeat, 0, 10);
         int volume = map(analogRead(5), 0, 1023, 127, 0);
+        volume = constrain(volume, 0, 127);
 
-        //Serial5.write(null);
-        Serial5.write(0x15);                                //Send speed command
-        Serial5.write(map(speechSpeed, 0, 127, 0x7F, 0x0)); //Send speed value mapped to hex
-        Serial5.write(0x16);                                //Send pitch command
-        Serial5.write(map(pitch, 0, 255, 0xFF, 0x0));       //Send pitch value mapped to hex
-        Serial5.write(0x17);                                //Send bend command
-        Serial5.write(map(bend, 0, 15, 0x0, 0xF));          //Send bend value mapped to hex
-        Serial5.write(map(phoneme, 0, 255, 0x0, 0xFF));     //Send phoneme value mapped to hex
-        Serial5.write(20);                                  // volume
-        Serial5.write(volume);
-
-        delay(delayLength); //Delay for however long the delay length is
-        while (digitalRead(2))
-                ;
-        while (digitalRead(PIN_SPEAKING))
-                ;
         switch (voiceState)
         {
-        case 0:
+        case 0: // idle
+                if (gated)
+                {
 
+                        //  Serial << "Gated " << endl;
+                        gated = false;
+                        Serial5.print("\\0"); // Entrer en Mode Controle SCP
+                        Serial5.print("R");   // READY
+                        Serial5.print("x");
+                        SpeakJet.write(0x15);        //Send speed command
+                        SpeakJet.write(speechSpeed); //Send speed value mapped to hex
+                        SpeakJet.write(0x16);        //Send pitch command
+                        SpeakJet.write(pitch);       //Send pitch value mapped to hex
+                        SpeakJet.write(0x17);        //Send bend command
+                        SpeakJet.write(bend);        //Send bend value mapped to hex
+                        SpeakJet.write(20);          // volume
+                        SpeakJet.write(volume);
+                        SpeakJet.write(26); // repeat
+                        SpeakJet.write(repeat);
+                        SpeakJet.write(phoneme); //Send phoneme value mapped to hex
+                        Serial << phonL[phoneme - 128].label << endl;
+                        Serial << repeat << endl;
+                        //Serial5.print("x")
+                       
+                }
+                break;
+        case 1:
+                if (digitalRead(GATE))
+                {
+                        Serial5.print("\\0"); // Entrer en Mode Controle SCP
+                        Serial5.print("S");   // READY
+                        Serial5.print("x");
+                        voiceState = 0;
+                }
                 break;
 
+        case 2: // done ?
+                break;
         default:
                 break;
         }
 }
 void getUser() // voir sinovox
 {
+        /*
+┌───┐
+│ 2 │
+│ 3 │
+│ 1 │
+└───┘
+*/
         mode = digitalRead(SW0) + digitalRead(SW1) * 2;
 }
 
@@ -240,11 +293,14 @@ void Buffer()
 {
         if (!digitalRead(PIN_BHF))
         {
+
                 digitalWrite(GREEN_LED, OFF);
+                digitalWrite(BUSY, OFF);
         }
         else
         {
                 digitalWrite(GREEN_LED, ON);
+                digitalWrite(BUSY, ON);
         }
 }
 
@@ -253,13 +309,13 @@ void Speaking()
         if (!digitalRead(PIN_SPEAKING))
         {
 
-                digitalWrite(BUSY, OFF);
+                //    digitalWrite(BUSY, OFF);
                 digitalWrite(RED_LED, OFF);
         }
         else
         {
 
-                digitalWrite(BUSY, ON);
+                //  digitalWrite(BUSY, ON);
                 digitalWrite(RED_LED, ON);
         }
 }
@@ -268,6 +324,11 @@ void justPressed()
 {
         //voir gerty2 pour le debounce
         pressed = true;
+}
+
+void justGated()
+{
+        gated = true;
 }
 
 /*
@@ -291,8 +352,9 @@ void setup()
         analogReadResolution(10);
 
         attachInterrupt(PIN_SPEAKING, Speaking, CHANGE);
-        attachInterrupt(PIN_BHF, Buffer, CHANGE);
+        attachInterrupt(PIN_BHF, Buffer, CHANGE); //TODO: why full in synth mode ?
         attachInterrupt(PUSH, justPressed, FALLING);
+        attachInterrupt(GATE, justGated, FALLING);
         attachInterrupt(ROTA, Rotary, CHANGE);
 
         //  button.attachDuringLongPress(longPress);
@@ -332,10 +394,15 @@ void loop()
 {
         getUser();
         button.tick();
+        //    Serial << "voice " << mainState << endl;
         if (button.getPressedTicks() > 500 && mainState)
         {
+                Serial5.print("\\0"); // Entrer en Mode Controle SCP
+                Serial5.print("W");   // READY
+                Serial5.print("x");
                 display.clear();
                 delay(400);
+                display.println(">");
                 interruptCount = 0;
                 mainState = 0;
                 rotF = 1;
@@ -343,27 +410,29 @@ void loop()
         }
         switch (mainState)
         {
-        case 0: // idle
-                if (rotF)
+        case 0:
+                if (rotF) // rotary is rotated
                 {
                         interruptCount = constrain(interruptCount, 0, numFunctions - 4);
                         function = interruptCount;
                         displayFunctionList(function);
                         rotF = 0;
                 }
-                if (pressed)
+                if (pressed) // function is selected
                 {
                         mainState = function + 1;
                         display.clear();
                         String str2 = mainFunctions[function];
                         display.print(str2);
                         pressed = false;
+                        voiceState = 0;
                 }
                 break;
         case 1: // voice synth
-                //  voiceSynth();
+
+                phonSynth();
                 break;
-        case 2: // synth
+        case 2: // sound synth
                 break;
         case 3: // code
                 break;
